30d36fb1e9ace5e07b518b9dd356dc3c42033b6d
raquenaengineering <raquenaengineering@gmail.com>
2023-02-01 16:26:21 +0100

    - Add method to find uti690 camera automatically.

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 
-
-
+import usb
 
 from uti690B import uti690B
 
 if __name__ == "__main__":
     print("Pene")
-
-thermal_cam = uti690B()
-thermal_cam.interactive_menu()
+    #
+    # import usb.core
+    # import usb.backend.libusb1
+    # import usb.util
+    #
+    # busses = usb.busses()
+    # for bus in busses:
+    #     devices = bus.devices
+    #     for dev in devices:
+    #         if dev != None:
+    #             try:
+    #                 xdev = usb.core.find(idVendor=dev.idVendor, idProduct=dev.idProduct)
+    #                 if xdev._manufacturer is None:
+    #                     xdev._manufacturer = usb.util.get_string(xdev, xdev.iManufacturer)
+    #                 if xdev._product is None:
+    #                     xdev._product = usb.util.get_string(xdev, xdev.iProduct)
+    #                 stx = '%6d %6d: ' + str(xdev._manufacturer).strip() + ' = ' + str(xdev._product).strip()
+    #                 print(stx % (dev.idVendor, dev.idProduct))
+    #             except:
+    #                 pass
 
--- uti690B.py
+++ uti690B.py
@@ -1,52 +1,91 @@
 # standard libraries #
 import logging
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level = logging.INFO)
 import time
 # pip installed libraries #
+import numpy
 import cv2
 
 # wrapper libraries, which require external installation #
-# import opencv
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 
-
+separator = "*************************************************************************"
 class uti690B():
 
 
 
     def __init__(self):
-        pass
+        self.camera_n = None
+
+
+    def find_camera(self):
+
+        # 1. find all available cameras (take a picture with each of them, until crash)
+        # 2. Check which camera has a picture size matching with the UTI690 resolution.
+        # 3. if no camera with the right size is found, return no uti690b found.
+        # if a camera is being used by another application, Videocapture works,
+
+
+        print("Finding camera number (this may take some seconds)")
+        done = False
+        camera_n = 0
+        retval = False
+        while(done == False):
+            logging.debug(separator)
+            # logging.debug("Camera number: " + str(camera_n))
+            camera = cv2.VideoCapture(camera_n)
+            # logging.debug("camera object created")
+            ok , img = camera.read()
+            logging.debug("return value camera read: " + str(ok))
+
+            if(ok == True):
+                logging.debug("Camera number: " + str(camera_n))
+                im_size = numpy.shape(img)
+                logging.debug("Picture size:" + str(im_size))
+                if(im_size == (321, 240, 3)):
+                    logging.debug("Picture dimensions matching UTi690")
+                    print("UTi690 camera found")
+                    self.camera_n = camera_n
+                    retval = True
+
+            else:
+                logging.debug("camera " + str(camera_n) + " not available?")
+
+            if(camera_n >= 100):
+                done = True
+
+            camera_n = camera_n + 1
+            logging.debug(separator)
+
+        if(retval == False):
+            print("No UTI690 was found, please review your connections")
+
+        return(retval)
 
     def interactive_menu(self):
         quit = 0
         while(quit == 0):
             option = input("Please choose option (write 'help' for command list)")
-            if(option == "preview"):
+            if(option == "find"):
+                found = self.find_camera()
+            elif(option == "preview"):
                 self.preview()
-            if(option == "snap"):
+            elif(option == "snap"):
                 self.take_snapshot()
-            if(option == "help"):
+            elif(option == "help"):
                 print("preview")
                 print("snap")
                 #print("split")
-            if(option == "test"):
+            elif(option == "test"):
                 self.test()
-            if(option == "quit"):
+            elif(option == "quit"):
                 quit = 1
             else:
                 print("Command not recognized")
 
-    def test(self):
-        img = self.take_snapshot()
-        images = self.split_image(img)
-        images_clean = self.clean_images(images)
-        self.save_images(images_clean)
-        vals = self.get_values_from_images(images_clean)
-
-
     def preview(self):
-        self.camera = cv2.VideoCapture(1)  # this should be variable.
+        self.camera = cv2.VideoCapture(self.camera_n)  # this should be variable.
         quit = 0
         while quit == 0:
             check, frame_thermal = self.camera.read()
@@ -65,7 +104,7 @@ class uti690B():
 
     def take_snapshot(self):            # stores picture to file
         try:
-            self.camera = cv2.VideoCapture(1)
+            self.camera = cv2.VideoCapture(self.camera_n)
         except:
             logging.warning("Camera not connected?")
             logging.warning("Camera already open?")
@@ -109,7 +148,6 @@ class uti690B():
         # 1. map the min_val to the lower value
         # 2. map the max_val to the higher value
 
-
     def save_images(self, image_list):
         for i in range(len(image_list)):
             im_name = "test_images/im" + str(i) + ".png"
@@ -129,7 +167,41 @@ class uti690B():
         return(values)
 
     def get_max_temp(self):             # finds max temp
-        pass
+        retval = False
+        max_temp_im = cv2.imread("test_images/max_temp.png")
+        max_temp = pytesseract.image_to_string(max_temp_im,config="--psm 8")  # read tesseract manual page, configuration for image as single word.
+        return(max_temp)
 
     def get_min_temp(self):             # min temp
-        pass
+        retval = False
+        min_temp_im = cv2.imread("test_images/min_temp.png")
+        min_temp = pytesseract.image_to_string(min_temp_im,config="--psm 8")  # read tesseract manual page, configuration for image as single word.
+        return(min_temp)
+
+    def test(self):
+
+        # camera_n = self.find_camera()
+        # if(camera_n):
+        #     print("UTI690B camera found, in n: " + str(self.camera_n))
+        # else:
+        #     print("No UTI cameras were found")
+
+        camera_found = self.find_camera()                                  # finding the camera
+
+        if (camera_found):
+            img = self.take_snapshot()
+            images = self.split_image(img)
+            images_clean = self.clean_images(images)
+            self.save_images(images_clean)
+            # vals = self.get_values_from_images(images_clean)
+            max_temp = self.get_max_temp()
+            print("Maximum temperature value: " + str(max_temp))
+            min_temp = self.get_min_temp()
+            print("Minimum temperature value: " + str(min_temp))
+        else:
+            print("NO CAMERA FOUND: Couldn't proceed with the testing")
+
+if __name__ == "__main__":
+    thermal_cam = uti690B()
+    thermal_cam.interactive_menu()
+
